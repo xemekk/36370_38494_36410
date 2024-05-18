53fa079c5ecd431c7af5eac98b2de3ed98a7cae1
GreenNick41 <148586704+GreenNick41@users.noreply.github.com>
2024-05-18 09:57:53 +0200

    Update calculator.py
    added calculate_nutrients

--- utils/calculator.py
+++ utils/calculator.py
@@ -7,3 +7,15 @@ def calculate_calories(products: dict) -> int:
         weight = products[product['name']]
         total_calories += product['cal'] * weight
     return total_calories
+
+def calculate_nutrients(products: dict) -> dict:
+    #example products = {'apple': 150} -> name: wieght
+
+    products_info = get_products_info(products.keys())
+    total_nutrients = defaultdict(float)
+    for product in products_info:
+        weight = products[product['name']]
+        for key, value in product.items():
+            if isinstance(value, (int, float)):
+                total_nutrients[key] += value * weight
+    return dict(total_nutrients)
