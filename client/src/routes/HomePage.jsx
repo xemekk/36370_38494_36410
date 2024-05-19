570695cca40716bc0414ff0dce1ff17b6b83213e
xemekk <xemronn@gmail.com>
2024-05-19 11:43:04 +0200

    fix text in homepage

--- client/src/routes/HomePage.jsx
+++ client/src/routes/HomePage.jsx
@@ -5,12 +5,11 @@ function HomePage() {
       <p className="mt-10 text-center">
         Our app helps you easily monitor your daily calorie intake and
         expenditure, empowering you to make informed dietary choices. With a
-        user-friendly interface, you can log meals, track nutritional
-        information, and set personalized goals to maintain a balanced diet.
-        Whether you&apos;re looking to lose weight, gain muscle, or simply
-        maintain a healthy lifestyle, Calories Calculator provides the tools and
-        insights you need to stay on track and reach your objectives with
-        confidence. Start your journey to better health today!
+        user-friendly interface, you can quickly check calorie intake for given
+        products. Whether you&apos;re looking to lose weight, gain muscle, or
+        simply maintain a healthy lifestyle, Calories Calculator provides the
+        tools and insights you need to stay on track and reach your objectives
+        with confidence. Start your journey to better health today!
       </p>
     </>
   );
