e6f3c11950be6ac7906e61f588afb178a9e7d41a
GreenNick41 <148586704+GreenNick41@users.noreply.github.com>
2024-06-13 13:47:44 +0200

    Update router.py

--- app/products/router.py
+++ app/products/router.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, HTTPException
-
+from fastapi import APIRouter, HTTPException, Query
+from typing import List
 from app.products.dao import ProductsDAO
 from app.products.schemas import SProducts
 
@@ -12,7 +12,7 @@ router = APIRouter(
 async def get_products() -> list[SProducts]:
     return await ProductsDAO.find_all()
 
-@router.get("/{product_id}")
+@router.get("/id/{product_id}")
 async def get_product_by_id(product_id: int) -> SProducts:
     result = await ProductsDAO.find_by_id(product_id)
     if result is None:
@@ -26,9 +26,21 @@ async def get_product_by_name(product_name: str) -> SProducts:
         raise HTTPException(status_code=404, detail="Product nie znaleziono")
     return result
 
+
+@router.get("/by_names")
+async def get_products_by_names(
+        names: List[str] = Query(..., min_length=1)
+) -> List[SProducts]:
+    results = await ProductsDAO.find_by_names(names=names)
+    if not results:
+        raise HTTPException(status_code=404, detail="Products not found")
+
+    return results
+
 @router.post("")
 async def add_product(name: str, calories: int, carbs: float = None, protein: float = None, fat: float = None):
-    await ProductsDAO.add(name=name, calories=calories, carbs=carbs, protein=protein, fat=fat)
+    result = await ProductsDAO.add(name=name, calories=calories, carbs=carbs, protein=protein, fat=fat)
+    return result
 
 @router.delete("/{product_id}")
 async def delete_product_by_id(product_id: int):
