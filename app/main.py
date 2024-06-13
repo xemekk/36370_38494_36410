15f893e805e174d7450cb7cb2e7ea1ffd8fe7e08
GreenNick41 <148586704+GreenNick41@users.noreply.github.com>
2024-06-13 12:03:51 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,4 +4,12 @@ from app.products.router import router as router_products
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router_products)
